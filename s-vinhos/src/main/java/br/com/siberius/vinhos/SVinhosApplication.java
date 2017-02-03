4c0161684d7d4f30a61f89a5f9662b66e2035b05
jhonnyscerni@gmail.com <jhonnyscerni@gmail.com>
2017-02-03 09:11:50 -0300

    Inicio do Projeto

---
+++ s-vinhos/src/main/java/br/com/siberius/vinhos/SVinhosApplication.java
@@ -0,0 +1,12 @@
+package br.com.siberius.vinhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SVinhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SVinhosApplication.class, args);
+	}
+}
---
+++ s-vinhos/src/test/java/br/com/siberius/vinhos/SVinhosApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.siberius.vinhos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SVinhosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
